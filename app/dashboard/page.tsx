aef8140b156dc5e336d3e082ee3f94d100d755b2
cyberdev <Gamerbob9393@gmail.com>
2024-07-07 18:06:34 -0700

    Added capability for appointments.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -23,11 +23,39 @@ export default async function Home() {
   const allUsers = await prisma.doctors.findMany({
     where: {email:session.user.email}
   })
+  const appointments = prisma.appointments.findMany({
+    where:{
+      doctor:session.user.email
+    }
+  })
+    
+  const currentDate = new Date();
+
+
   return (
     <div className="flex h-screen bg-black">
       <div className="w-screen h-screen flex flex-col space-y-5 justify-center items-center">
         <SignOut />
-        <p className="text-stone-400 hover:text-stone-200 transition-all">Your patients:</p>
+
+        <p className="text-orange-400">
+          APPOINTMENTS {' -- '}
+
+      {`${currentDate.getMonth() + 1}/${currentDate.getDate()}/${currentDate.getFullYear()}`}
+
+      <br></br>
+      
+      {(await appointments).map((data) => (
+        <p className="text-yellow-400 hover:text-yellow-200 transition-all">
+
+          { ? (return 'ok')}
+          
+          </p>
+      ))}
+      </p>
+
+
+
+        <p className="text-stone-400 hover:text-stone-200 transition-all">All your patients:</p>
         {allUsers.map((data) => (
           <a
           className="text-stone-400 hover:text-stone-200 transition-all"
